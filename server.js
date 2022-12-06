d9520baf0f33a094f4b467644d1184fcba38bf01
kumarprashant24 <kumar.prashant@tothenew.com>
2022-12-06 14:40:57 +0530

    chaged file structure

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
-const transactionalRecords = require('./app/restful/routes/transactionalRecords')
+const transactionalRecords = require('./src/app/restful/routes/transactionalRecords')
 const bodyParser = require('body-parser')
 const app = express()
 const PORT = process.env.PORT || 5000
@@ -16,7 +16,7 @@ app.use(
 )
 app.get('/', (req, res) => {
 
-  // res.send("hello word");
+  res.send("hello word");
  
 });
 app.use('/api/account-service/tab/accounts', transactionalRecords)
